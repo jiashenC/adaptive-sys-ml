58fe4387800083bd9c56fc7804299e9c317d4753
jiashenC <caojiashen24@gmail.com>
2018-11-25 14:39:40 -0500

    [Update] initial commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# adaptive-sys-ml
+# Adaptive Machine Learning on IoT Devices
+This repos serves as a demo for running deep learning neural network models on IoT devices. The goal 
+of this demo is to show that with adaptive strategy implemented on IoT devices, the framework is able 
+to preserve better data integrity with the similar performance.
---
+++ client.py
@@ -0,0 +1,39 @@
+import time
+from threading import Thread
+import os
+
+import avro.ipc as ipc
+import avro.protocol as protocol
+import numpy as np
+
+PATH = os.path.abspath(__file__)
+DIR_PATH = os.path.dirname(PATH)
+
+# data packet format definition
+PROTOCOL = protocol.parse(open(DIR_PATH + '/resource/message/message.avpr').read())
+
+
+def send_request(frame, id):
+    client = ipc.HTTPTransceiver('192.168.1.14', 12345)
+    requestor = ipc.Requestor(PROTOCOL, client)
+
+    data = dict()
+    data['input'] = frame.astype(np.float32).tobytes()
+    data['identifier'] = id
+
+    requestor.request('forward', data)
+
+
+def master():
+    data = np.random.random_sample([100])
+    for i in range(100):
+        Thread(target=send_request, args=(data, i,)).start()
+        time.sleep(0.01)
+
+
+def main():
+    master()
+
+
+if __name__ == '__main__':
+    main()
